a7f147b322ed63227aaca41fa12388f3c08458ae
guodong <gd@tongji.edu.cn>
2020-07-08 11:46:40 +0800

    init

---
+++ main.py
@@ -0,0 +1,67 @@
+from z3 import *
+
+u_a_1 = Bool("u_a_1")
+v_a_1 = Bool('v_a_1')
+u_a_2 = Bool("u_a_2")
+v_a_2 = Bool('v_a_2')
+
+u_b_1 = Bool("u_b_1")
+v_b_1 = Bool('v_b_1')
+u_b_2 = Bool("u_b_2")
+v_b_2 = Bool('v_b_2')
+
+u_c_1 = Bool("u_c_1")
+v_c_1 = Bool('v_c_1')
+u_c_2 = Bool("u_c_2")
+v_c_2 = Bool('v_c_2')
+
+u_d_1 = Bool("u_d_1")
+v_d_1 = Bool('v_d_1')
+u_d_2 = Bool("u_d_2")
+v_d_2 = Bool('v_d_2')
+
+u_e_1 = Bool("u_e_1")
+v_e_1 = Bool('v_e_1')
+u_e_2 = Bool("u_e_2")
+v_e_2 = Bool('v_e_2')
+
+s = Solver()
+s.add(Or([And([u_a_1, u_b_1, u_c_1]), And([v_a_1, v_b_1, v_c_1])]))
+s.add(Or([And([u_a_2, u_b_2, u_c_2]), And([v_a_2, v_b_2, v_c_2])]))
+
+s.add(Or([And([u_d_1, u_e_1]), And([v_d_1, v_e_1])]))
+s.add(Or([And([u_d_2, u_e_2]), And([v_d_2, v_e_2])]))
+
+s.add(Or([And([u_d_1, u_c_1]), And([v_d_1, v_c_1])]))
+s.add(Or([And([u_d_2, u_c_2]), And([v_d_2, v_c_2])]))
+
+s.add(Or([Not(Or([And([u_a_1, u_d_1]), And([v_a_1, v_d_1])])), Not(Or([And([u_a_2, u_d_2]), And([v_a_2, v_d_2])]))]))
+
+s.add(Or([Not(Or([And([u_c_1, u_e_1]), And([v_c_1, v_e_1])])), Not(Or([And([u_c_2, u_e_2]), And([v_c_2, v_e_2])]))]))
+
+s.check()
+model = s.model()
+print model[u_a_1]
+
+
+
+def getBit(u, v):
+    if u == False and v == True:
+        return '0'
+    elif u == True and v == False:
+        return '1'
+    elif u == True and v == True:
+        return '*'
+    else:
+        return 'z'
+
+
+def get(u1, v1, u2, v2):
+    return getBit(u1, v1) + getBit(u2, v2)
+
+
+print 'a:' + get(model[u_a_1], model[v_a_1], model[u_a_2], model[v_a_2])
+print 'b:' + get(model[u_b_1], model[v_b_1], model[u_b_2], model[v_b_2])
+print 'c:' + get(model[u_c_1], model[v_c_1], model[u_c_2], model[v_c_2])
+print 'd:' + get(model[u_d_1], model[v_d_1], model[u_d_2], model[v_d_2])
+print 'e:' + get(model[u_e_1], model[v_e_1], model[u_e_2], model[v_e_2])
---
+++ pe.py
@@ -0,0 +1,103 @@
+import networkx as nx
+from z3 import *
+import itertools
+import threading
+import multiprocessing
+import time
+
+file = '4323:4323/edges'
+
+
+def main(N, N1):
+    s = Solver()
+    g = nx.DiGraph()
+    with open(file) as f:
+        lid = 0
+        for l in f:
+            src = l.split(' -> ')[0]
+            dst = ' '.join(l.split(' -> ')[1].split(' ')[:-1])
+            if not g.has_edge(src, dst):
+                code = []
+                for i in range(0, N):
+                    code.append((Bool('u' + str(lid) + str(i)), Bool('v' + str(lid) + str(i))))
+                lid = lid + 1
+                g.add_edge(src, dst, c=code)
+
+
+                code1 = []
+                for i in range(0, N):
+                    code1.append((Bool('u' + str(lid) + str(i)), Bool('v' + str(lid) + str(i))))
+                lid = lid + 1
+                g.add_edge(dst, src, c=code1)
+
+    print len(g.nodes)
+    print len(g.edges)
+
+
+    maxD = 0
+    for n in g.nodes:
+        if g.in_degree(n) > maxD:
+            maxD = g.in_degree(n)
+
+    print maxD
+    return
+
+    npaths = 30000
+    paths = []
+    out_edges = set([])
+    for n1 in g.nodes:
+        for n2 in g.nodes:
+            if len(paths) == npaths:
+                break
+            if (n1 != n2):
+                try:
+                    path = nx.shortest_path(g, n1, n2)
+                    paths.append(path)
+
+                    # print path
+                    edges = zip(path[:-1], path[1:])
+                    out_edges = out_edges.union(edges)
+                    codes = map(lambda x: g.get_edge_data(x[0], x[1])['c'], edges)
+                    # print codes
+                    for i in range(0, N):
+                        s.add(Or([And(map(lambda x: x[i][0], codes)), And(map(lambda x: x[i][1], codes))]))
+                    # s.add(Or([And(map(lambda x: x[1][0], codes)), And(map(lambda x: x[1][1], codes))]))
+
+
+                except:
+                    1
+                    print 'nopath'
+
+    print len(paths)
+    nn = 0
+    for n in g.nodes:
+        if g.out_degree(n) > 1:
+            edges = g.out_edges(n)
+
+            for es in itertools.combinations(edges, 2):
+                if es[0] in out_edges and es[1] in out_edges:
+                    codes = map(lambda x: g.get_edge_data(x[0], x[1])['c'], es)
+                    nn += 1
+                    # s.add(Or([Not(Or([And(map(lambda y: y[0][0], codes)), And(map(lambda y: y[0][1], codes))])), Not(Or([And(map(lambda y: y[1][0], codes)), And(map(lambda y: y[1][1], codes))]))]))
+                    # s.add(Or(map(lambda i: Not(Or([And(map(lambda y: y[i][0], codes)), And(map(lambda y: y[i][1], codes))])), range(0, N))))
+                    s.add(Not(And(map(lambda i: Or([And(codes[0][i][0], codes[1][i][0]), And(codes[0][i][1], codes[1][i][1])]), range(0, N)))))
+                else:
+                    pass
+                    # print 'not out port'
+    m = s.check()
+    print '----'
+    print N
+    print m
+
+# main(2)
+
+threads = []
+for i in range(6, 10):
+    thread = multiprocessing.Process(target=main, args=(i,i))
+    threads.append(thread)
+    thread.start()
+
+# for t in threads:
+#     t.start()
+
+time.sleep(1000000)
